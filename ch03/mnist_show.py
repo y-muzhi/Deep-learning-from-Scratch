5654cb82f91d85a94fbc761db394dae8ad8d0a83
羊牧之 <muzhi@y-MacBook-Pro.local>
2026-01-29 20:23:19 +0800

    Add ch03

---
+++ ch03/mnist_show.py
@@ -0,0 +1,25 @@
+# ch03/mnist_show.py
+import sys, os
+sys.path.append(os.pardir)
+
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+img = x_train[0]
+label = t_train[0]
+print(label)  # 例如：5
+
+print(img.shape)           # (784,)
+img = img.reshape(28, 28)  # 还原为 28x28
+print(img.shape)           # (28, 28)
+
+img_show(img)
